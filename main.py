c3ea98cc8681e33a5e51cfa5a98eb4f6788773eb
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-01-23 21:31:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,23 +28,59 @@ AutoCaptionBotV1 = pyrogram.Client(
     name="AutoCaptionBotV1", api_id=app_id, api_hash=api_hash, bot_token=bot_token
 )
 
+start_message = """
+<b>👋Hello {}</b>
+<b>I am an AutoCaption bot</b>
+<b>All you have to do is add me to your channel and I will show you my power</b>
+<b>Maintained By : @Illegal_Developer</b>"""
+
+about_message = """
+<b>• Name : <a href=https://t.me/Illegal_Developer>AutoCaption</a></b>
+<b>• Developer : <a href=https://t.me/Illegal_Developer>ɪʟʟᴇɢᴀʟ ᴅᴇᴠᴇʟᴏᴘᴇʀꜱ</a></b>
+<b>• Language : Python3</b>
+<b>• Library : Pyrogram v{version}</b>
+<b>• Updates : <a href=https://t.me/Illegal_Developer>Click Here</a></b>"""
+
 @AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
 def start_command(bot, update):
     update.reply(
-        "👋 Hello! I'm your AutoCaption bot.",
+        start_message.format(update.from_user.mention),
+        reply_markup=start_buttons(bot, update),
         parse_mode=pyrogram.enums.ParseMode.HTML,
+        disable_web_page_preview=True,
+    )
+
+@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("start"))
+def strat_callback(bot, update):
+    update.message.edit(
+        start_message.format(update.from_user.mention),
+        reply_markup=start_buttons(bot, update.message),
+        parse_mode=pyrogram.enums.ParseMode.HTML,
+        disable_web_page_preview=True,
+    )
+
+@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("about"))
+def about_callback(bot, update):
+    bot = bot.get_me()
+    update.message.edit(
+        about_message.format(
+            version=pyrogram.__version__, username=bot.mention
+        ),
+        reply_markup=about_buttons(bot, update.message),
+        parse_mode=pyrogram.enums.ParseMode.HTML,
+        disable_web_page_preview=True,
     )
 
 @AutoCaptionBotV1.on_message(pyrogram.filters.channel)
 def edit_caption(bot, update: pyrogram.types.Message):
-    motech, file_id = get_file_details(update)
+    motech, _ = get_file_details(update)
     try:
         if motech:
             try:
-                update.edit(custom_caption.format(file_name=file_id))
+                update.edit(custom_caption.format(file_name=motech.file_name))
             except pyrogram.errors.FloodWait as FloodWait:
                 asyncio.sleep(FloodWait.value)
-                update.edit(custom_caption.format(file_name=file_id))
+                update.edit(custom_caption.format(file_name=motech.file_name))
     except pyrogram.errors.MessageNotModified:
         pass
 
@@ -73,26 +109,51 @@ def handle_custom_message(bot, update):
         bot.send_message(chat_id, f"Error during find and replace: {str(e)}")
 
 def get_file_details(update: pyrogram.types.Message):
-    if update.photo:
-        # If it's a photo, use the file_id as the unique identifier
-        return update.photo, update.photo.file_id
-
-    for message_type in (
-        "animation",
-        "audio",
-        "document",
-        "video",
-        "video_note",
-        "voice",
-        "sticker",
-    ):
-        obj = getattr(update, message_type, None)
-        if obj:
-            return obj, obj.file_id
+    if update.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker",
+        ):
+            obj = getattr(update, message_type, None)
+            if obj:
+                return obj, obj.file_id
 
     return None, None
 
-print("Telegram AutoCaption Bot Start")
+def start_buttons(bot, update):
+    bot = bot.get_me()
+    buttons = [
+        [
+            pyrogram.types.InlineKeyboardButton(
+                "Updates", url="https://t.me/Illegal_Developer"
+            ),
+            pyrogram.types.InlineKeyboardButton(
+                "About 🤠", callback_data="about"
+            ),
+        ],
+        [
+            pyrogram.types.InlineKeyboardButton(
+                "➕️ Add To Your Channel ➕️",
+                url=f"http://t.me/{bot.username}?startchannel=true",
+            )
+        ],
+    ]
+    return pyrogram.types.InlineKeyboardMarkup(buttons)
 
-if __name__ == "__main__":
-    AutoCaptionBotV1.run()
+def about_buttons(bot, update):
+    buttons = [
+        [
+            pyrogram.types.InlineKeyboardButton(
+                "🏠 Back To Home 🏠", callback_data="start"
+            )
+        ]
+    ]
+    return pyrogram.types.InlineKeyboardMarkup(buttons)
+
+print("Telegram AutoCaption
